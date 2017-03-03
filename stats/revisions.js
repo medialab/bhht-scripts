9812acbbfd4c9b3ab05766ebb04fe7ce141e1680
Yomguithereal <guillaumeplique@gmail.com>
2017-03-03 15:36:17 +0100

    Dumb query

--- stats/revisions.js
+++ stats/revisions.js
@@ -9,7 +9,16 @@ const mysql = require('mysql'),
       config = require('./config.json');
 
 const CONNECTION = mysql.createConnection({
-  host: 'localhost',
+  host: 'enwiki.labsdb',
+  database: 'enwiki_p',
   user: config.user,
   password: config.password
 });
+
+CONNECTION.connect();
+
+CONNECTION.query('SELECT * FROM revision LIMIT 10', (err, results) => {
+  console.log(err, results);
+});
+
+CONNECTION.end();
