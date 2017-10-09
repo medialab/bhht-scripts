fce2b4558a1d6391e9149dc9d812f3b57a55b745
Yomguithereal <guillaumeplique@gmail.com>
2017-10-09 18:18:47 +0200

    Working

--- mining/extract_links.py
+++ mining/extract_links.py
@@ -8,11 +8,22 @@
 import sys
 import signal
 import zlib
+import pandas as pd
 from bs4 import BeautifulSoup
 from progressbar import ProgressBar
 from multiprocessing import Pool
-from config import PROCESSORS, MONGODB
+from config import DATA, PROCESSORS, MONGODB
 from pymongo import MongoClient
+
+# TODO: césar (gallery), Alexander_the_Great (iconography)
+
+# Building the location set
+# df = pd.read_csv(DATA['location'], usecols=[0], dtype={0: str}, engine='c')
+
+LOCATIONS = set()
+
+# for _, row in df.iterrows():
+#     LOCATIONS.add(row['location'])
 
 # Mongo connection
 mongo_client = MongoClient(MONGODB['host'], MONGODB['port'])
@@ -20,10 +31,12 @@ db = mongo_client.bhht
 collection = db.people
 
 # Predicates
-QUERY = {'done': True}
+QUERY = {'done': True, '_id': 'fr§Louis_XVI'}
 
 # Process
 def extract_links(doc):
+
+    # TODO: add a links filter here for good measure
     if (
         ('notFound' in doc and doc['notFound']) or
         ('badRequest' in doc and doc['badRequest'])
@@ -32,6 +45,32 @@ def extract_links(doc):
 
     html = zlib.decompress(doc['html']).decode('utf-8')
     soup = BeautifulSoup(html, 'lxml')
+
+    content = soup.find(id='content')
+    all_links = content.find_all('a')
+
+    relevant_links = set()
+
+    for link in all_links:
+        href = link.get('href')
+
+        if not href:
+            continue
+
+        # Keeping only wiki links
+        if not href.startswith('/wiki/'):
+            continue
+
+        href = href.split('/wiki/')[1].strip()
+
+        # Keeping only locations
+        # if href not in LOCATIONS:
+        #     continue
+
+        relevant_links.add(href)
+
+    for link in relevant_links:
+        print(link)
 
     return True
 
