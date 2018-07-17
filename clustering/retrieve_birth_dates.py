5e2fe772a9fe79b6ae843cc0c814a6ea0d81a558
Yomguithereal <guillaumeplique@gmail.com>
2018-07-17 17:49:15 +0200

    Working

---
+++ clustering/retrieve_birth_dates.py
@@ -0,0 +1,44 @@
+import csv
+import requests
+import sys
+
+INPUT = sys.argv[1]
+OUTPUT = 'birthdates.csv'
+
+BASE_URL = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&props=claims'
+
+
+def create_url(lang, name):
+    return '%s&sites=%swiki&titles=%s' % (BASE_URL, lang, name)
+
+
+with open(INPUT, 'r') as input_file, \
+     open(OUTPUT, 'w') as outputfile:
+     reader = csv.DictReader(input_file)
+     writer = csv.DictWriter(outputfile, fieldnames=['name', 'lang', 'birth_date'])
+     writer.writeheader()
+
+     for line in reader:
+        url = create_url(line['lang'], line['name'])
+        print(url)
+
+        r = requests.get(url)
+
+        data = r.json()
+
+        # Finding the correct claim
+        claims = next(iter(data['entities'].values())).get('claims')
+        birthdate = None
+
+        if claims is not None:
+
+            birthdate_claim = next((v for k, v in claims.items() if k == 'P569'), None)
+
+            if birthdate_claim is not None:
+                birthdate = birthdate_claim[0]['mainsnak']['datavalue']['value']['time'].lstrip('+').split('T', 1)[0]
+
+        writer.writerow({
+            'lang': line['lang'],
+            'name': line['name'],
+            'birth_date': birthdate if birthdate is not None else ''
+        })
